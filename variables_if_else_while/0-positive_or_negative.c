0390ee8bf91dffb0666f255e9569d2c3090483d6
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-16 09:49:39 -0800

    C

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n > 0)
 {
-printf("is positive %d\n", n);
+printf( "%d is positive \n", n);
 }
 if(n < 0)
 {
-printf("is negative %d\n", n);
+printf( "%d is negative\n", n);
 }
- else
+ if(== 0)
 {
-printf("is zero %d\n", n);
+printf( "%d is zero\n", n);
 }
 return (0);
 }
