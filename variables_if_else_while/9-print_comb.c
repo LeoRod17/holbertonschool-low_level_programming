e4508673b52571306bde8f87217d0672e654ba5a
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-17 08:51:01 -0800

    C

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+int c=44;
+for(i = 48; i < 58; i++)
+{
+putchar(i);
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
