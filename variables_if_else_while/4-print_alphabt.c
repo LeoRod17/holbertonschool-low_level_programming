e088192e4277e01e2b2f07079dfb0c9f2b3bd70c
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-17 05:46:22 -0800

    C

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123 i++)
+{
+if(i == 65 || i == 71)
+{
+i++;
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
