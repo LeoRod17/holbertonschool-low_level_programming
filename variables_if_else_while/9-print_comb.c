15258d5db119661b1be57f4f2fa83044ea7be2c5
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-17 08:53:17 -0800

    C

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@
 int main(void)
 {
 int i;
-int c=44;
-for(i = 48; i < 58; i++)
+int c = 44;
+for (i = 48; i < 58; i++)
 {
 putchar(i);
 putchar(c);
+putchar('\n')
 }
-putchar('\n');
 return (0);
 }
