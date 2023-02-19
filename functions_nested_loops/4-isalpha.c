b9733b015fb5032ddb6b879483dced7b3656fb42
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-19 13:35:08 -0800

    C

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,15 +2,18 @@
 #include <ctype.h>
 /**
  *_isalpha - Indicates if is a lleter or somethig else
- *
+ *@c: - the number I recieve from main
  *Return: 0
  */
 
 int _isalpha(int c)
 {
 if (isalpha(c) != 0)
-return 1;
-else {
-return 0;
+{
+return (1);
+}
+else
+{
+return (0);
 }
 }
