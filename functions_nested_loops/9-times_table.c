b19d84970ca63c2561e09e0002d90f821491579c
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-19 14:13:02 -0800

    c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#includes "main.h"
+
+/**
+ *times_table - Prints the multiplication table
+ *
+ *Return: 0
+ */
+
+void times_table(void)
+{
+int a = 0;
+int x = 0;
+int res;
+for (;x < 9; x++)
+{
+for(;a < 9; a++)
+{
+res=a*x;
+putchar(res);
+}
+putchar('\n');
+}
+return (0);
+}
