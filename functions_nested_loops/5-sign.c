fb0514de67b62af6d0aa4e17ddc480bb70750a16
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-19 13:55:10 -0800

    C

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,17 +10,16 @@ int print_sign(int n)
 if(n < 0)
 {
 putchar(45);
-putchar(49);
+return (1);
 }
-if (n > 0)
+ else if (n > 0)
 {
 putchar(43);
- putchar(49);
+return (1);
 }
-if (n == 0)
+else
 {
 putchar(48);
-putchar(48);
-}
 return (0);
+}
 }
