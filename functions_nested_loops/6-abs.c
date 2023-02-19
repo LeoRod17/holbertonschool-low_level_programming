13bb40a8eb0feb1a1b3dd28d0b87583a3110f94f
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-19 10:22:38 -0800

    abs

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,11 +3,13 @@
 /**
  *_abs - we use it to get the absolute value of an interger
  *
+ *x - is the value that I receive
+ *
  *Return: 0
  */
 
 int _abs(int x)
 {
 int a = abs(x);
- return (a);
+return (a);
 }
