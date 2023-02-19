7c461820d32fbbe53d6eab7f8f5a4dcfb5b85934
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-19 11:06:04 -0800

    C

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,39 +8,8 @@
 
 
 int _putchar(char c){
-int i;
-for (i = 95; i != 115; i++)
-{
-putchar(i);
-if (i == 95)
-{
-i = 111;
-}
-if (i == 116)
-{
-i=98;
-}
-if (i == 112)
-{
-i = 116;
-}
-if (i == 117)
-{
-i = 115;
-}
-if (i == 99)
-{
-i = 103;
-}
-if (i == 104)
-{
-i = 96;
-}
-if (i == 97)
-{
-i = 113;
-}
-}
+putchar(c);
+
 putchar('\n');
  return (0);
 }
