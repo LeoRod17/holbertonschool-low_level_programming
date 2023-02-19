05d9363da28c469337e72866cf43ed5c16e0a004
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-19 14:24:54 -0800

    last

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_last_digit - this prints the las digit of a number
+ *@num: - the number which I would get the last digit
+ *Return: 0
+ */
+
+int print_last_digit(int num);
+{
+int last;
+last = num % 10;
+return (last);
+}
