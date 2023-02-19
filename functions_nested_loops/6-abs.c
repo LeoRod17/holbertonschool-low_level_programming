486cf07c8c078bc5557eeed63d437cd39586ecb3
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-19 10:25:28 -0800

    abs

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *_abs(int x) - we use it to get the absolute value of an interger
+ *_abs(int c) - we use it to get the absolute value of an interger
  *
- * x - is the value that I receive
+ * c - is the value that I receive
  *
  *Return: 0
  */
 
-int _abs(int x)
+int _abs(int c)
 {
-int a = abs(x);
+int a = abs(c);
 return (a);
 }
