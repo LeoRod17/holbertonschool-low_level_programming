5b2cd57821fa6796fe7961319b46f81e4e44013d
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-22 08:58:17 -0800

    multi

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ *mul - what mul is going to do is to multiplicate a and b
+ *@a: - a number that is going to be multiplied
+ *@b: - a number that is going to be multiplicated with a
+ *Return: 0
+ */
+
+int mul(int a, int b)
+{
+int res;
+res = a * b;
+return (res);
+}
