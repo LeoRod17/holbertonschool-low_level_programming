ecc9f822f888342ced1f9940cdcddb92d6da0e02
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-22 09:42:54 -0800

    numbs

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *more_numbers - this print from 0-14 ten times
+ *
+ *Return: 0
+ */
+
+void more_numbers(void)
+{
+int i;
+int a;
+int x=0;
+while(x<9)
+{
+for (a = 48; a < 50)
+{
+for (i = 48; i < 58; i++)
+{
+_puthcar(a);
+_putchar(i);
+if(a == 49 && i == 52)
+{
+a=50;
+i=60;
+}
+}
+}
+x++;
+}
+putchar('\n');
+}
