8ce8bb36ffb0ed082aa24d60eb838e3151521c6c
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-22 09:54:26 -0800

    numb

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,21 +10,21 @@ void more_numbers(void)
 int i;
 int a;
 int x = 0;
-while (x<9)
+while (x < 9)
 {
 for (a = 48; a < 50; a++)
 {
 for (i = 48; i < 58; i++)
 {
-if (a== 49)
+if (a == 49)
 {
 _putchar(a);
 }
 _putchar(i);
 if (a == 49 && i == 52)
 {
-a=50;
-i=60;
+a = 50;
+i = 60;
 }
 }
 _putchar('\n');
