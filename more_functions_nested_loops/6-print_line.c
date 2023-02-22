f2bf0d124b349cfea4b5afb4bd8bfea8db93e6e4
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-22 10:09:38 -0800

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_line - a function that prints a line as long as the variable
+ *@n: -  the times the line will be printed
+ *
+ *Return: 0
+ */
+
+void print_line(int n)
+{
+for(n; n > 0 n --)
+{
+_putchar(95);
+}
+_putchar(' ')
+}
