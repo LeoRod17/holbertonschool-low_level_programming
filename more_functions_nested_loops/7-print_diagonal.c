d53940982f399ae9f6917083dc72af28da26d846
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-23 04:10:42 -0800

    sidebar

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#includes "main.h"
+/**
+ *print_diagonal
+ *
+ *Return:
+ */
+
+void print_diagonal(int n)
+{
+int a;
+for (a = 1; a <= n; a++)
+{
+_putchar(92);
+}
+if (n > 0)
+{
+_putchar('\n');
+}
+if (n <= 0)
+{
+_putchar('\n');
+}
+}
