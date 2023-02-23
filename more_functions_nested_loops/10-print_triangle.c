8137b5d55cdd81f57e037c44b723421de1dc7289
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-23 09:22:42 -0800

    tri

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,6 +11,12 @@ void print_triangle(int size)
 int a;
 int b;
 int s = size;
+if(size <= 0)
+{
+_putchar('\n');
+}
+else
+{
 for(a = 1; a <= size; a++)
 {
 for (b = 1; b < a; b++)
@@ -27,8 +33,5 @@ s--;
 _putchar('\n');
 }
 }
-if(size <= 0)
-{
-_putchar('\n');
 }
 }
