a2570f3c85fc959fb6b321c3fd07992a151568f8
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-23 05:41:28 -0800

    loops

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_square - print the symbol # in a square form
+ *@size: - the value of how much # will print
+ *Return: 0
+ */
+
+void print_square(int size)
+{
+int a;
+for (a = 0; a < size; a++)
+{
+_putchar(35);
+_putchar('\n');
+if (size <= 0)
+{
+_putchar('\n');
+}
+}
+}
