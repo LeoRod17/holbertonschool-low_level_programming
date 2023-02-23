eafd6a9d75ac4f65e6b7d2fcfd5cf1edcee87ac5
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-23 05:55:45 -0800

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,16 +9,12 @@ void print_square(int size)
 {
 int a;
 int b;
-for (b = 1; b < size; b++)
+for (b = 0; b < size; b++)
 {
-_putchar('\n');
-for (a = 1; a < size; a++)
+for (a = 0; a < size; a++)
 {
 _putchar(35);
-if (size <= 0)
-{
 _putchar('\n');
-}
 }
 }
 }
