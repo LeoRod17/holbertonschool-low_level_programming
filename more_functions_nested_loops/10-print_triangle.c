5ddb8ef1d6fb9f5d8cc51c3b5b4ff3bf59775301
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-23 09:16:43 -0800

    tri

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *print_triangle - it prints a triangle with the # simbol
+ *@size: - the size of the variable
+ *Return: 0
+ */
+
+void print_triangle(int size)
+{
+int a;
+int b;
+int s = size;
+for(a = 1; a <= size; a++)
+{
+for (b = 1; b < a; b++)
+{
+if(j >= s -1)
+{
+_putchar(35);
+}
+else
+{
+putchar(' ');
+}
+s--;
+_putchar('\n');
+}
+}
+if(size <= 0)
+{
+_putchar('\n');
+}
+}
