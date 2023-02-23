240de94e76dc3f1c5448116a3d222c268d42ea4d
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-23 07:42:23 -0800

    Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *main - entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 99; i++)
+{
+if (i % 3 == 0)
+{
+put("Fizz");
+_putchar(' ');
+}
+if else (i % 5 == 0)
+{
+put("Buzz");
+_putchar(' ');
+}
+if else (i % 3 == 0 && i % 5 == 0)
+}
+put("FizzBuzz");
+_putchar(' ');
+}
+else
+{
+printf("%d", i);
+_putchar(' ');
+}
+}
+}
