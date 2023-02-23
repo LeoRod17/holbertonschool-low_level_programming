9a60c891081dea1e44dfae05dc64214308b3689d
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-23 08:31:13 -0800

    buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,21 @@ for (i = 1; i < 101; i++)
 if(i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
-putchar(' ');
 }
 else if (i % 3 == 0)
 {
 printf("Fizz");
-putchar(' ');
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
-putchar(' ');
 }
 else
 {
 printf("%d", i);
+}
+if(i<100)
+{
 putchar(' ');
 }
 }
