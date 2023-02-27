536e976f7cc780e2fdd1faf3ced8f173e02cd432
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-27 09:19:49 -0800

    array

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,13 @@ void rev_string(char *s)
 {
 int a;
 int b;
+int c=0;
 a = strlen(s);
 char rev[1000];
 for (b = a - 1; b >= 0; b--)
 {
-rev = s[b];
+rev[c] == s[b];
+c++;
 }
-s = rev;
+s == rev;
 }
