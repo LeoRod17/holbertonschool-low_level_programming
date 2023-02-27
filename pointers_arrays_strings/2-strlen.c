5acf3c57f4095b4dcb2a9d7726cbf23b60656b8c
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-27 07:30:04 -0800

    pointers

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen - a function that shows the prints the lenght of the array
+ *
+ *@s: - the array 
+ */
+
+int _strlen(char *s);
+{
+int a;
+a = strlen(*s);
+return (a);
+}
