45c77bf5a5387d11a2d7a264bd661557c58ec907
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-27 06:25:50 -0800

    pointers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - it is a function to swap the number of two int pointers
+ *@a: - the first value to change
+ *@b: - the second value to change
+ *Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int *swap;
+*swap = a;
+*a = *b;
+*b = *swap; 
+}
