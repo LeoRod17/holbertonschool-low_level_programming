f4407b1360d06dde59d49a840d1ba9f8784d21d8
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-28 06:13:17 -0800

    arrays

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "string.h"
+/**
+ **_strcpy - a function that copy the array to another
+ *@dest: - the destiny to copy the array
+ *@src: - the origin of the array
+ *Return: a char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(src,dest);
+return (dest);
+}
