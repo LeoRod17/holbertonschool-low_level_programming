b5358464d24470eee8b882cb5a8f7290a00200e9
LeoRod17 <Matiassotelo171097@gmail.com>
2023-02-28 05:06:58 -0800

    arrays

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *print_array
+ *@n: - the value of integer where i start
+ *@a: the pointer to the array
+ */
+
+void print_array(int *a, int n)
+{
+int s;
+s = strlen(a);
+for (; n < s; n++)
+{
+putchar("%d, "a[n]);
+}
+}
