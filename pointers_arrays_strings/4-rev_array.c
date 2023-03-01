75146c228b4f769ada7bdc1920d9f19b8b5aeaf4
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-01 07:18:32 -0800

    loops

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *reverse_array
+ *@a: - the string 
+ *@n: - the size of the string
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+int i;
+int s;
+int b;
+char r[1000];
+b = 0;
+for (s = n - 1; s >= 0; s++)
+{
+r[b] = a[s];
+b++;
+}
+for (i= 0; i <= n;i++)
+{
+a[i] = r[i];
+}
+}
