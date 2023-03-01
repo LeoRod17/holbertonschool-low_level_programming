baf188f5498ca134add61cbea205a0f85a1f1ed2
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-01 06:20:47 -0800

    loops

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -15,24 +15,41 @@ int i;
 int r;
 int d;
 int s;
+d = 0;
 s = strlen(s1);
 r = strlen(s2);
 if(s1 > s2)
 {
 for(i = 0; i < s; i++)
 {
+if(s1[i] != s2[i])
+{
 a = s1[i]-'0';
 b = s2[i]-'0';
 d = a - b;
+break;
+}
+else
+{
+d = 0;
+}
 }
 }
 if(s2 > s1)
 {
 for(i = 0; i < r; i++)
 {
+if(s1[i] != s2[i])
+{
 a = s1[i]-'0';
 b = s2[i]-'0';
 d = a - b;
+break;
+}
+else
+{
+d = 0;
+}
 }
 }
 return (d); 
