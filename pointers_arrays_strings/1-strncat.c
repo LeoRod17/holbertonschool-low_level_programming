48622cd4c8a12be0a0a61ac3e46e1192fcb0de0d
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-01 04:13:36 -0800

    pointers

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -5,15 +5,22 @@
  *@dest: - the value of an first array
  *@src: - the value of the second array
  *@n: - the value of the length of the array
+ *Return - dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
 int s;
 int i;
+int a;
 s = strlen(dest);
+a = strlen(src);
 for (i = 0; i < n; i++)
 {
+if (n > a)
+{
+n = a
+}
 dest[s] = src[i];
 s++;
 }
