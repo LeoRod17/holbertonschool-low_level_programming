c5eeae2ad461f190e81ef3ed4da4a69a99800da1
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-01 04:37:42 -0800

    pointers

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ **_strncpy - a function that copy the two string n times
+ *@dest: - the value of the return 
+ *@src: - the variable of the source
+ *@n: - the value of how much to copy
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+int s;
+s = strlen(dest);
+for(i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+for (; n < s; n++)
+{
+dest[n] = 0;
+}
+}
