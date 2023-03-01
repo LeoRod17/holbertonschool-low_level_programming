06405f1c1ff0c1d6dd363774d7e8a331c6d86797
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-01 09:02:03 -0800

    bugs

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -13,6 +13,7 @@ int s;
 int b;
 int i;
 int array[20];
+ b = 0;
 for (s = n - 1; s >= 0; s--)
 {
 array[b] = a[s];
---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ **string_toupper - a function that changed the lower cas to uppercase
+ *@s: - the pointer to the string 
+ *
+ */
+
+char *string_toupper(char *s)
+{
+
+}
