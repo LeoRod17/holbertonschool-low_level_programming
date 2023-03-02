27db0ddf196edd86d831d25339ad12a283d4a4b1
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-02 13:13:43 -0800

    strings

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 
 /**
  *
- * *_memcpy: - a function that copies a pointer memory area
+ **_memcpy - a function that copies a pointer memory area
  *@dest: - the pointer of the array that will be copied
  *@src: - the pointer of the array source
  *@n: - the amount of space that will be replaced
