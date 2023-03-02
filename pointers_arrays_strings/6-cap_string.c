973a72133c99e2fb87e6ff9170dde4c0e84e4119
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-02 04:20:01 -0800

    7loop

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,19 +12,20 @@ int i;
 int a;
 int b;
 int c;
- char arr[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+char arr[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 a = strlen(s);
 for (i = 0; i < a; i++)
 {
-  n = i - 1;
-  for(c = 0; c < 9; c++)
-   {
-     if(n == arr[c])
-       {
-	 b = s[i];
-	 b = b - 32;
-	 s[i] = b;
-       }
-   }
+n = i - 1;
+for(c = 0; c < 9; c++)
+{
+if(n == arr[c])
+{
+b = s[i];
+b = b - 32;
+s[i] = b;
 }
+}
+}
+return (s);
 }
