d6696ca78305340e4f5eedbaf61cb2181566b19e
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-03 08:35:43 -0800

    5-string

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <string.h>
+/**
+ **_strstr - a function that prints after the located array
+ *@haystack: - the string to try looking for 
+ *@needle: - the pointer 
+ *Return: needle
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+int a;
+int b;
+int c;
+int d = 0;
+char *p;
+a = strlen(haystack);
+b = strlen(needle);
+for (i = 0; i < a; i++)
+{
+if (haystack[i] == needle[d])
+{
+c = i;
+for (d = 0; d <= b; d++)
+{
+if (haystack[c] != needle[d])
+{
+break;
+}
+c++;
+if (d == b)
+{
+p = &haystack[i];
+}
+else
+{
+return (0);
+}
+}
+}
+}
+return (p);
+}
