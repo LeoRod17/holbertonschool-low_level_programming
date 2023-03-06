0d7161a3b54c2638689b92c6713cd287dde3ae01
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-06 05:55:45 -0800

    1 recur

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - a function that returns the size of a string
+ *@s: - the pointer to the string
+ *
+ */
+int _strlen_recursion(char *s)
+{
+if (s != '\0')
+{
+_strlen_recursion(s + 1);
+}
+else
+{
+int r = *s;
+return r;
+}
+}
