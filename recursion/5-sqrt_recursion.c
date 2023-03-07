4a7d8a060abec561719f8490eb9a82f214b11986
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-07 05:23:49 -0800

    5 rec

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_raiz: - a function to calculate the value of the root
+ *_raiz - a function to calculate the value of the root
  *@n: - the number to do the natural square root
  *@r: - the value of the result of the root
  *Return: an int
@@ -19,7 +19,7 @@ return (_raiz(n, r + 1));
 }
 
 /**
- *_sqrt_recursion - a function to get the square root of an int 
+ *_sqrt_recursion - a function to get the square root of an int
  *@n: - the number to do the natural square root
  *Return: an int
  */
