c3aa9b27c7d8d6116347f07dd4c60a1628e17904
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-07 13:37:45 -0800

    1-args

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *main - entry point of the function
+ *@argc: - the count of the arguments 
+ *@argv: - the arguments
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+  int i;
+  for (i = 0; i < argc, i++)
+    {
+      printf("%d" argv[i]);
+    }
+}
---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - entry point
+ *@argc: - the number of arguments that we have
+ *@argv: - the arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+
+}
+printf("%d", i);
+_putchar('\n');
+return (0);
+}
