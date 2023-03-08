b756fbcccd23d77037a5b471d3fc12643820e517
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-08 10:08:51 -0800

    4-alpha

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
 #include <ctype.h>
+
+/**
+ *oddone - a function to look every caracter of the string
+ *@alpha: - the string I want to check
+ *Return: 0 or 1
+ */
+int oddone(char *alpha)
+{
+int j;
+int s;
+int a;
+s = strlen(alpha);
+for (j = 0; j < s; j++)
+{
+a = isalpha(alpha[j]);
+if (a != 0)
+{
+return (1);
+}
+}
+return (0);
+}
+
 /**
  *main - entry point
  *@argc: - number of arguments
@@ -12,18 +35,12 @@ int main(int argc, char *argv[])
 int i;
 int a;
 int r;
-if (argc < 0)
-{
-puts("Error");
-return (1);
-}
-if (argc == 0)
-{
-puts("0");
-}
+int s;
+r = 0;
 for (i = 1; i < argc; i++)
 {
-if (isalpha(*argv[i]) > 0)
+s = oddone(argv[i]);
+if (s != 0)
 {
 puts("Error");
 return (1);
