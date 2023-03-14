1aca893d386058ade438af780a5e265dca979db5
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-14 05:47:29 -0700

    0-MMalloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ **malloc_checked: - a function to do malloc without malloc 
+ *@b: - the size of the variable
+ *
+ *Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+unsigned int i;
+char *a;
+
+if (b == 0)
+{
+_putchar(57);
+_putchar(56);
+}
+for (i = 0; i < b; i++)
+{
+}
+}
