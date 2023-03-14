d68ac904109987202edcf3c334b669be0e7f5493
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-14 08:50:58 -0700

    1-MMalocs

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ **string_nconcat - a function to concatenate two strings
+ *@s1: - first string
+ *@s2: - second string
+ *@n: - how much of the second array will i print
+ *Return a pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i;
+unsigned int a;
+unsigned int b;
+unsigned int j;
+char *p;
+
+if(s1 == NULL && s2 == NULL)
+{
+return(NULL);
+}
+a = strlen(s1);
+b = strlen(s2);
+if (n > b)
+{
+n = b;
+}
+p = malloc(sizeof(int) * (a + n));
+if (p == NULL)
+{
+exit(1);
+}
+for (i = 0; i < a; i++)
+{
+p[i] = s1[i];
+}
+for (j = 0; j < n; j++)
+{
+p[i] = s2[j];
+i++;
+}
+return (p);
+}
