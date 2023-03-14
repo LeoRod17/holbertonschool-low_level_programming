f084befe052086044505162967a976b7df4eb161
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-14 10:41:39 -0700

    MalArray

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ **array_range - a function to do an array
+ *@min: -  the value with the array will start
+ *@max: - the value of te max of the array
+ *
+ */
+int *array_range(int min, int max)
+{
+int *a;
+int i;
+if (min > max)
+{
+return (NULL);
+}
+ a = malloc(sizeof(int) * (max + 1));
+if (a == NULL)
+{
+exit(1);
+}
+i = min;
+for(; i <= max; i++)
+{
+a[i] = i;
+}
+return (a);
+}
