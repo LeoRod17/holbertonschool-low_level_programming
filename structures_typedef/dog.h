4bd57d2d10829c33d8ec0e761ce70c77a7df0a44
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-20 06:41:43 -0700

    0-DOG

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef _dog_
 #define _dog_
+/**
+ *struct dog - an structure based on a dog
+ *@name: - the name of the dog
+ *@age: - the age of the dog
+ *@owner: - the owner of the dog
+ */
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-}dog;
+} dog;
 #endif
