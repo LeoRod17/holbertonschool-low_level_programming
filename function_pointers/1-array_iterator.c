59568c5c1f8b67ce3e477c43eedb8ad974405f3f
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-21 15:36:09 -0700

    1-FP

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- *array_iterator - a function to execute a function in every part of an array 
+ *array_iterator - a function to execute a function in every part of an array
  *@array: - the array to work with
  *@size: - the size of the array
  *@action: - the pointer of the function to use
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 unsigned int i;
 if (array != NULL && size > 0 && action != NULL)
 {
-for (i = 0; i< size; i++)
+for (i = 0; i < size; i++)
 {
 action(array[i]);
 }
