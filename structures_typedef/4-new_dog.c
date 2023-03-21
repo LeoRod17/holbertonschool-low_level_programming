af534f7001a42a62c6713a395d8d4764f85191c9
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-21 05:50:10 -0700

    GOD-DOG

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -5,28 +5,32 @@
  *new_dog - a function to add a dog to the sistem
  *@name: - the name of the dog
  *@age: - the age of the dog
- *@owner - the owner of the dog
+ *@owner: - the owner of the dog
+ *Return: a dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
-if(name == NULL || age < 0 || owner == NULL)
+if (name == NULL || age < 0 || owner == NULL)
 {
 return (NULL);
 }
 d = malloc(sizeof(dog_t));
-if(d == NULL)
+if (d == NULL)
 {
+free(d);
 return (NULL);
 }
 name = malloc(sizeof(name));
 if (name == NULL)
 {
+free(name);
 return (NULL);
 }
 owner = malloc(sizeof(owner));
-if(owner == NULL)
+if (owner == NULL)
 {
+free(owner);
 return (NULL);
 }
 d->name = name;
