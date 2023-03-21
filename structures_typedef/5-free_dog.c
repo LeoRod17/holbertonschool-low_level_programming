075e00d397b53e8949c776e0b9f8f0b73f1b91fe
LeoRod17 <Matiassotelo171097@gmail.com>
2023-03-21 06:37:44 -0700

    FREE THE DOGSSSS

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 
 /**
- *free_dog: - a function to free a dog
- *d: - the dog to be freed
+ *free_dog - a function to free a dog
+ *@d: - the dog to be freed
  *Return: 0
  */
 void free_dog(dog_t *d)
 {
+if(d != NULL)
+{
 free(d->name);
 free(d->owner);
 free(d);
+}
 }
