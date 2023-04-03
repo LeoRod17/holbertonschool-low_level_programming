4ed72b56afc559ec63758e77a5283f898d37237b
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-03 09:35:34 -0700

    1-lists

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *list_len
+ *@h: the list of elements
+ *Return: the lenght of elements
+ */
+size_t list_len(const list_t *h)
+{
+size_t ele = 0;
+
+while(h != NULL)
+{
+ele ++;
+h = h->next;
+}
+return (ele);
+}
