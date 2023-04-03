ffb4d9ec5debbb164eda97a965d53f6c18ae6a50
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-03 08:54:22 -0700

    0-Print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *print_list - a function that prints a singel lined lists
+ *@h: the list to print
+ *Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t p = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("(%d)", h->len);
+			printf("%s\n", h->str);
+		}
+		else
+		{
+			printf("[0]");
+			printf("(nil)\n");
+		}
+		p++;
+		h = h->next;
+	}
+	return (p);
+}
