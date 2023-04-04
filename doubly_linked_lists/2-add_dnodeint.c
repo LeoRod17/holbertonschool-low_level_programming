7478b8c9f7fc97ca19d70b5a4e854203cc136fb3
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-04 06:37:19 -0700

    2-add node

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - 'a function that adds a node to the list.'
+ * @head: the list which I am going to add the node
+ * @n: what i am going to add in the node
+ * Return: a pointer
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *list;
+
+list = malloc(sizeof(dlistint_t));
+if (list == NULL)
+{
+	free(list);
+return (*head);
+}
+else
+{
+list->n = n;
+list->next = *head;
+*head = list;
+}
+return (*head);
+}
