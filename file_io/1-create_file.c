3689c3f78d68166a53a6eb01a4b1f02c7dedcd99
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-13 09:39:15 -0700

    1-create file v4

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,16 +8,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, r, w;
+	int o, w;
 
 	o = open(filename,  O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (o == -1)
 		return (-1);
 
-	r = read(o, text_content, strlen(text_content));
-	if (r == -1)
-		return (-1);
-	w = write(o, text_content, r);
+	w = write(o, text_content, strlen(text_content));
 	if (w == -1)
 		return (-1);
 
