7349e49821e216a1f8568547a5fd9b9a101ce88b
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-13 09:50:44 -0700

    1-create file v7

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,6 +12,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if(text_content == NULL)
+		return (1);
 
 	o = open(filename,  O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (o == -1)
