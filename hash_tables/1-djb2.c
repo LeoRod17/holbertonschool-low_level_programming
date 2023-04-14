be83b59b63184a57c0e8a5159326dfdf34f5e7a5
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-14 09:35:52 -0700

    1-djb2 betty hotfix

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -4,7 +4,7 @@
  *   * @str: string used to generate hash value
  *    *
  *     * Return: hash value
- *      */
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
