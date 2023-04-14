4a05ea79dac00371fa788c93f972d6fd10320fc0
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-14 08:01:18 -0700

    0-init hash v3

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,13 +13,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
-		free(table);
 		return (NULL);
 	}
 	table->array = malloc(sizeof(hash_node_t) * size);
 	if (table->array == NULL)
 	{
-		free(table->array);
 		free(table);
 		return (NULL);
 	}
@@ -29,5 +27,4 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	table->size = size;
 	return (table);
-
 }
