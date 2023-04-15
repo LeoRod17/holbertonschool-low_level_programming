d76017abcd86196c280199fb4d08b74463f1516f
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-15 13:07:10 -0700

    3-add elem v2

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,6 +3,7 @@
 /**
  *hash_table_set - a function to add an element to a hash table
  *@ht: the hash table
+
  *@key: the key of the new node
  *@value: the value of the new node
  *Return: a hash table
@@ -27,9 +28,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	h->key = strcpy(h->key, key);
 	h->value = strcpy(h->value, value);
-	res = hash_djb2((unsigned char *)key) % ht->size;
+	res = key_index((unsigned char *)key, ht->size);
 	while (ht->array[res] != NULL)
 	{
+		if (ht->array[res]->key == key)
+		{
+			ht->array[res] = h; 
+		}
 		res++;
 		res %= ht->size;
 	}
---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ **hash_table_get - a function that retrieves a value of key
+ *@ht: the hash table
+ *@key: the key of what you are looking for
+ *Return the table
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+}
