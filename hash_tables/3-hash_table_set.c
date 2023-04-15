b340995e7532644f8c279e3149ce958d1fb875f7
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-15 12:08:21 -0700

    3-add elem v1

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_set - a function to add an element to a hash table
+ *@ht: the hash table
+ *@key: the key of the new node
+ *@value: the value of the new node
+ *Return: a hash table
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+	hash_node_t *h;
+	unsigned long int res;
+
+	if (ht == NULL)
+		return (0);
+	if (key == NULL)
+		return (0);
+	if (value == NULL)
+		value = "";
+	h = malloc(sizeof(hash_node_t) * ht->size);
+	if (h == NULL)
+	{
+		free(h);
+		return (0);
+	}
+	h->key = strcpy(h->key, key);
+	h->value = strcpy(h->value, value);
+	res = hash_djb2((unsigned char *)key) % ht->size;
+	while (ht->array[res] != NULL)
+	{
+		res++;
+		res %= ht->size;
+	}
+	ht->array[res] = h;
+
+	return (1);
+}
