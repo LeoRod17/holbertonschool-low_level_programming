f42c358aa6b8427590bcc0c1c3840bf665a13eb7
LeoRod17 <Matiassotelo171097@gmail.com>
2023-04-16 15:16:10 -0700

    hash print v2

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,14 +7,26 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-unsigned long int i;
-
+unsigned long int i,j = 0;
+hash_node_t *node;
 if (ht != NULL)
 {
+	printf("{");
 for (i = 0; i < ht->size; i++)
 {
-	printf(" '%s': ", ht->array[i]->key);
-	printf("'%s', ", ht->array[i]->value);
+	node = ht->array[i];
+	while(node != NULL)
+	{
+		if(j == 1)
+		{
+			printf(", ");
+		}
+	printf(" '%s': ", node->key);
+	printf("'%s'", node->value);
+	node = node->next;
+	j = 1;
+	}
 }
+printf("}\n");
 }
 }
