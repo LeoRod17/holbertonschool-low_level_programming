3139f33be9d2abb815e6e3c60fac4dd2dd9d6b1f
root <matiassotelo171097@gmail.com>
2023-05-26 09:36:05 -0700

    searching 2-6

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ *linear_search -searches a value in an array using the Linear search algorithm
+ *@array: is a pointer to the first element of the array to search in
+ *@size:  the number of elements in array
+ *@value:  is the value to search for
+ *Return:
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+/*Libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+/*Prototypes*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
