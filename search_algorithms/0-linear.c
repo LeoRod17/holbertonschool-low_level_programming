098aa200c179672209100020bf14aa0f92b99919
LeoRod17 <matiassotelo171097@gmail.com>
2023-05-27 09:35:49 -0700

    betty hotfix of searching algorithms

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
 
 /**
- *linear_search -searches a value in an array using the Linear search algorithm
+ *linear_search - search a value in an array using the Linear search algorithm
  *@array: is a pointer to the first element of the array to search in
  *@size:  the number of elements in array
  *@value:  is the value to search for
- *Return:
+ *Return: the index were the number was found
  */
 int linear_search(int *array, size_t size, int value)
 {
@@ -13,19 +13,19 @@ size_t i;
 
 if (array == NULL)
 {
-    return (-1);
+	return (-1);
 }
 
 for (i = 0; i < size; i++)
 {
-    printf("Value checked array[%ld]",i);
-    printf(" = [%d]\n", array[i]);
+	printf("Value checked array[%ld]", i);
+	printf(" = [%d]\n", array[i]);
 
-    if (array[i] == value)
-    {
-        return (i);
-    }
-    
+	if (array[i] == value)
+	{
+		return (i);
+	}
+
 }
 
 return (-1);
