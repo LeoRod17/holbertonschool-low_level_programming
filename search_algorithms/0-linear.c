35c6403ae7b65f01e023e0391910b015439276c7
LeoRod17 <matiassotelo171097@gmail.com>
2023-05-27 09:29:25 -0700

    search algorithm 0-linear v1 & fix O-files

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,5 +9,24 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+size_t i;
 
+if (array == NULL)
+{
+    return (-1);
+}
+
+for (i = 0; i < size; i++)
+{
+    printf("Value checked array[%ld]",i);
+    printf(" = [%d]\n", array[i]);
+
+    if (array[i] == value)
+    {
+        return (i);
+    }
+    
+}
+
+return (-1);
 }
